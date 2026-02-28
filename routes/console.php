cc24aac5ffca95d74d8dad7e6a5bfd5160855b6c
Raíssa Dias <raissablw@gmail.com>
2026-02-28 12:56:20 -0300

    fix(back-end): remover zips de deploy

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('bot:comment-news')
+    ->everyTwoHours()
+    ->between('08:00', '23:59')
+    ->withoutOverlapping();
